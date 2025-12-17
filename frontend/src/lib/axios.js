4ae2656478c2f542b2352ec9596045b94e1bd306
Bereket-Eshete <bereketeshete63@gmail.com>
2025-12-17 08:28:45 +0300

    remove the backend package.json file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: import.meta.mode === "development" ? "https://mern-ecommerce-ou8n.onrender.com/api" : "/api",
 	withCredentials: true, // send cookies to the server
 });
 
