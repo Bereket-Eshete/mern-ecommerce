11025e7c218630c8e6351b3214e3f601759c8991
Bereket-Eshete <bereketeshete63@gmail.com>
2025-12-17 22:00:56 +0300

    preapre the frontend fro deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "https://mern-ecommerce-2-5seq.onrender.com/api",
+	baseURL: import.meta.env.VITE_API_URL || "https://mern-ecommerce-2-5seq.onrender.com/api",
 	withCredentials: true, // send cookies to the server
 });
 
